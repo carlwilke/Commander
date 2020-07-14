6b8d08ec436a1835d6cbbd1d9d44af751a16d753
Carl Wilke <carlwilke@outlook.com>
2020-07-14 12:59:54 +0100

    Updated Delete Command

--- Data/MockCommanderRepo.cs
+++ Data/MockCommanderRepo.cs
@@ -39,5 +39,10 @@ namespace Commander.Data
         {
             throw new NotImplementedException();
         }
+
+        public void DeleteCommand(Command cmd)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
